c05ab6a8ac85c53ae0179868176f8e909dcc734d
xzzx <youxiang0411@qq.com>
2020-04-29 09:52:17 +0800

    新增仪表盘

--- tools.js
+++ tools.js
@@ -251,7 +251,7 @@ dynamicLoading.js('https://www.layuicdn.com/layui/layui.js', () => {
             });
             if (!arr.length) {
               layer.msg('请选择人员');
-              return;
+              return false;
             }
             arr.forEach((item, index) => {
               (function (item, index) {
